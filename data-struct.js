d70435b4bf0d900dd98c37eab2d01e9710016edc
Joel Vazquez <joelomar09@gmail.com>
2016-09-20 21:09:29 -0400

    uodate url db

--- data-struct.js
+++ data-struct.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
     app = require('./server'),
-    url = 'mongodb://localhost:27017/app_data';
+    url = 'mongodb://joelomar:devel0p3r@ds035826.mlab.com:35826/private_chat';
 
 mongoose.connect(url);
 
